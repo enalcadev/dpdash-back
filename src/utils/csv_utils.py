12338a844d7b4d48c4638310e45269a371b9a876
Oriol Lamelas I Sanchez <o.lamelasisanchez@dxc.com>
2024-04-25 13:47:50 +0200

    DPD-3: Added real case test file, date parsing test done

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -5,6 +5,11 @@ def csv_to_usable_data(file):
     return pd.read_csv(file)
 
 
+def customize_date_column_format(data, column, initial_format, end_format):
+    data[column] = pd.to_datetime(data[column], format=initial_format)
+    data[column] = data[column].dt.strftime(end_format)
+
+
 def read_csv_rows(file_rows):
     for row in file_rows:
         print('\n'.join(row))
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,15 +1,25 @@
 import os
 import unittest
 import pandas as pd
+from src.utils import csv_utils
+
+
+def get_full_file(path) -> str:
+    script_dir = os.path.dirname(__file__)
+    return str(os.path.join(script_dir, path))
 
 
 class TestUtils(unittest.TestCase):
     def test_get_results_from_data(self):
-        script_dir = os.path.dirname(__file__)
-        full_path = os.path.join(script_dir, 'files/characters.csv')
-        data = pd.read_csv(full_path)
+        data = pd.read_csv(get_full_file(path='files/characters.csv'))
         self.assertTrue((data.columns == ['Type', 'Name', 'Birthday', 'Kind', 'Colour']).all)
         self.assertTrue((data["Colour"].value_counts(normalize=True) * 100).round(2)["Orange"] == 66.67)
+
+    def test_convert_date_field_to_date(self):
+        data = pd.read_csv(get_full_file(path='files/worklog-details-report202404250906.csv'))
+        self.assertTrue((data.columns == ["Assignee", "Worklog Description", "User", "Start Time", "Time Spent"]).all)
+        csv_utils.customize_date_column_format(data, "Start Time", "%d/%m/%Y %H:%M %p", "%d/%m/%Y")
+        self.assertEqual(data["Start Time"][197], '06/03/2024')
 
 
 if __name__ == '__main__':
