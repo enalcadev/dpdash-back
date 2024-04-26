ac710577de9dfd08799500d231f7c13c737ad559
Oriol Lamelas I Sanchez <o.lamelasisanchez@dxc.com>
2024-04-26 10:15:35 +0200

    DPD-3: Identify low frequency results in column by dynamic pct, with test

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -12,3 +12,8 @@ def customize_date_column_format(data, column, initial_format, end_format):
 
 def get_total_sum_by_id(data, id_column, to_sum_column):
     return data.groupby(id_column).sum()[to_sum_column].reset_index()
+
+
+def identify_low_frequency_results(data, column, pct):
+    res = (data[column].value_counts(normalize=True) * 100).round(2)
+    return res[res < pct].index
--- test/test_utils.py
+++ test/test_utils.py
@@ -28,6 +28,25 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(csv_utils.get_total_sum_by_id(data, "Assignee", "Time Spent (s)").loc[3]["Time Spent (s)"],
                          50400)
 
+    def test_identify_low_frequency_results(self):
+        data = pd.read_csv(get_full_file(path='files/worklog-details-report202404250906.csv'))
+        self.assertTrue((csv_utils.identify_low_frequency_results(data, "Worklog Description", 1) ==
+                         ['Geoc actualizado', 'SGE listo', 'GOVER funcionando',
+                          'Hores empleadas en la configuració de les VPN y GEOC',
+                          'Configurar proyecto GOVR', 'GOVER', 'SAI', 'RITC',
+                          'Ayudando a Geoffrey a configurar GEOC',
+                          'Soporte a Jonathan con configuración SGE+', 'Hores empleadas en GEOC',
+                          'SGE/GOVER', 'Horas empleadas en la confirguración de VPN Gene',
+                          'Horas empleadas en GEOC', 'Configuración de GEOC', 'GOVR + SGE',
+                          'He estado mirando que todos los proyectos funcionaran, y me puse también a instalar GEOC',
+                          'ICT / GOVR', 'FORMATEO', 'ASISTENCIA TECNICA',
+                          'GOVR. Muchos problemas con base de datos',
+                          'Ayuda sge, se ha conseguido acabar de instalar',
+                          'Se realizo la Miració de ICT', 'Migració de ICT',
+                          'Problemas con la Migració de ICT',
+                          'Una serie de problemas con la migracion en RITC solucionados',
+                          'Migracion de RITC', 'GEOC - Working']).all())
+
 
 if __name__ == '__main__':
     unittest.main()
