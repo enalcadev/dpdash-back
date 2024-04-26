69bce75a6ae57b2665f98f6e283c1d2efa191799
Oriol Lamelas I Sanchez <o.lamelasisanchez@dxc.com>
2024-04-26 10:16:10 +0200

    DPD-3: Remove unused method

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -1,10 +1,6 @@
 import pandas as pd
 
 
-def csv_to_usable_data(file):
-    return pd.read_csv(file)
-
-
 def customize_date_column_format(data, column, initial_format, end_format):
     data[column] = pd.to_datetime(data[column], format=initial_format)
     data[column] = data[column].dt.strftime(end_format)
